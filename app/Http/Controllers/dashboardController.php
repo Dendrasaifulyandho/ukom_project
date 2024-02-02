6cdc6f2f928a3bbef233e4c14aff47de0a22f47e
M-Arsya <arsyayaeger@gmail.com>
2024-02-02 13:10:57 +0700

    add dashboard admin

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -12,7 +12,7 @@ class dashboardController extends Controller
     public function index()
     {
         //
-        return view('dashboard.index');
+        return view('dashboardpm.index');
     }
 
     /**
